2f8bf1cec2b6726d8bbfad9ba1dca5cd7b6ac388
cheatdestroy <cheatdestroy@yandex.com>
2022-10-05 23:42:17 +0300

    Added new helper methods

---
+++ ChatBot.Anonymous/Common/Helpers/ArgumentHelper.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace ChatBot.Anonymous.Common.Helpers
+{
+    /// <summary>
+    /// Вспомогательные команды для валидации аргументов
+    /// </summary>
+    public static class Argument
+    {
+        /// <summary>
+        /// Проверяет значение на null
+        /// </summary>
+        /// <remarks> При указании параметров message, chatId и botClient отправляется сообщение пользователю в Telegram </remarks>
+        /// <param name="value"> Проверяемое значение </param>
+        /// <param name="message"> Сообщение при исключении </param>
+        /// <param name="chatId"> Уникальный идентификатор чата </param>
+        /// <param name="botClient"> Клиент бота для отправки сообщения </param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"> Если значение null, пробрасывается исключение </exception>
+        public static async Task NotNull<T>(
+            [NotNull] T? value, 
+            string message, 
+            long chatId,
+            ITelegramBotClient botClient,
+            [CallerArgumentExpression(parameterName: "value")] string? paramName = null)
+        {
+            if (value == null)
+            {
+                if (!string.IsNullOrEmpty(message) && botClient != null)
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: chatId, 
+                        text: message, 
+                        parseMode: ParseMode.Markdown);
+                }
+
+                throw new ArgumentNullException(paramName, message);
+            }
+        }
+
+        /// <summary>
+        /// Проверяет значение на null
+        /// </summary>
+        /// <param name="value"> Проверяемое значение </param>
+        /// <param name="message"> Сообщение при исключении </param>
+        /// <exception cref="ArgumentNullException"> Если значение null, пробрасывается исключение </exception>
+        public static void NotNull<T>(
+            [NotNull] T? value,
+            string message,
+            [CallerArgumentExpression(parameterName: "value")] string? paramName = null)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+        }
+
+        public static async Task OutOfRange(
+            int value,
+            int maxValue,
+            int minValue,
+            string message,
+            long chatId,
+            ITelegramBotClient botClient,
+            [CallerArgumentExpression(parameterName: "value")] string? paramName = null)
+        {
+            if (maxValue < value || value < minValue)
+            {
+                if (!string.IsNullOrEmpty(message) && botClient != null)
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: message,
+                        parseMode: ParseMode.Markdown);
+                }
+
+                throw new ArgumentNullException(paramName, message);
+            }
+        }
+        
+        public static async Task OutOfRange(
+            int value,
+            int maxValue,
+            int minValue,
+            string message,
+            [CallerArgumentExpression(parameterName: "value")] string? paramName = null)
+        {
+            if (maxValue < value || value < minValue)
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+        }
+    }
+}
--- ChatBot.Anonymous/Common/Helpers/EnumsHelper.cs
+++ ChatBot.Anonymous/Common/Helpers/EnumsHelper.cs
@@ -1,4 +1,7 @@
-﻿namespace ChatBot.Anonymous.Common.Helpers
+﻿using ChatBot.Anonymous.Common.Enums;
+using System.ComponentModel;
+
+namespace ChatBot.Anonymous.Common.Helpers
 {
     /// <summary>
     /// Вспомогательные функции для перечислений
@@ -6,25 +9,69 @@
     public static class EnumsHelper
     {
         /// <summary>
-        /// Проверяет на отсутствие указанного числа в перечислении
+        /// Переводит тип данных int в указанный enum
         /// </summary>
-        /// <param name="value"> Число </param>
-        /// <param name="enumType"> Перечисление </param>
-        /// <returns> Возвращает true, если указанное число отсуствует в перечислении; иначе false </returns>
-        public static bool IsNotOwnerValue(this int? value, Type enumType)
+        /// <typeparam name="T"> Перечесление </typeparam>
+        /// <param name="value"> Значение </param>
+        /// <returns> Возвращает значение перечисления, если оно присутствует в указанном перечислении; иначе null </returns>
+        public static T? ToEnum<T>(this int value) where T : struct, Enum
         {
-            return (!value.HasValue || !Enum.IsDefined(enumType, value));
+            if (!Enum.IsDefined(typeof(T), value))
+            {
+                return null;
+            }
+
+            return (T?)Enum.ToObject(typeof(T), value);
         }
 
         /// <summary>
-        /// Проверяет на отсутствие указанного числа в перечислении
+        /// Переводит тип данных int? в указанный enum
         /// </summary>
-        /// <param name="value"> Число </param>
-        /// <param name="enumType"> Перечисление </param>
-        /// <returns> Возвращает true, если указанное число отсуствует в перечислении; иначе false </returns>
-        public static bool IsNotOwnerValue(this int value, Type enumType)
+        /// <typeparam name="T"> Перечесление </typeparam>
+        /// <param name="value"> Значение </param>
+        /// <returns> Возвращает значение перечисления, если оно присутствует в указанном перечислении; иначе null </returns>
+        public static T? ToEnum<T>(this int? value) where T : struct, Enum
         {
-            return !Enum.IsDefined(enumType, value);
+            if (!value.HasValue)
+            {
+                return null;
+            }
+
+            return value.Value.ToEnum<T>();
+        }
+
+        /// <summary>
+        /// Получает описание перечислений
+        /// </summary>
+        /// <param name="value"> Значение перечисления </param>
+        /// <param name="defaultValue"> Значение по умолчанию, если атрибут отсутствует или значение равно null </param>
+        /// <returns> Возвращает описание перечисления </returns>
+        public static string GetDescription(this Enum? value, string defaultValue = "неизвестно")
+        {
+            if (value == null)
+            {
+                return defaultValue;
+            }
+
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+
+            if (name != null)
+            {
+                var field = type.GetField(name);
+
+                if (field != null)
+                {
+                    var attr = (DescriptionAttribute?)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+
+            return defaultValue;
         }
     }
 }
